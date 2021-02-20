8dd5e723aec7a0e99ca44e8e45255a82cc88f15d
zipudhe <lucasramalho29@gmail.com>
2021-02-20 20:49:52 -0300

    :tada: Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd8a8bc1b9aa8631602a837e5392855ab'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0080e8e9-c0e0-4a4d-9946-e8c7eea2520e'
+};
